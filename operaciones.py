11ebd0c5a4ab59c96d54dd748485e63071886257
Kevin <kevinacontrerasl62@gmail.com>
2020-12-05 22:27:46 -0600

    OPERACIONES

--- operaciones.py
+++ operaciones.py
@@ -1,12 +1,16 @@
 def calculos (url):
   contador=0;
   contador2=0;
+  contador3 =0;
   promedioEdad=0;
+  promedioSalario=0;
   trabajos =[]
   trabajo2 = []
   arregloFinal1 = []
   arregloCandidatos = []
   arregloEdad =[]
+  arregloSalario =[]
+  todoslosDatos = []
 
   for arreglo in url:
     trabajos.append(arreglo[4])
@@ -20,17 +24,24 @@ def calculos (url):
         contador=contador+1;
         contador2= contador2 + int(arreglo3[3])
         promedioEdad = contador2/contador
+        contador3= contador3 + int(arreglo3[5].strip("\n"))
+        promedioSalario = contador3/contador
+        
     """ PERSONAS REGISTRADAS PARA CADA PUESTO"""
     arregloCandidatos.append(contador)
     arregloEdad.append(promedioEdad)  
-    """---------------------------------------"""
-
+    arregloSalario.append(promedioSalario)
     contador=0
     contador2=0;
     promedioEdad=0;
-  print(arregloFinal1)
-  print(arregloCandidatos)
-  print(arregloEdad)
-
+    contador3=0;
+    promedioSalario=0;
+  todoslosDatos.append(arregloCandidatos)
+  todoslosDatos.append(arregloEdad)
+  todoslosDatos.append(arregloSalario)
+  todoslosDatos.append(trabajo2)
+  """---------------------------------------"""
+  print(todoslosDatos)
+  return todoslosDatos
    
 
