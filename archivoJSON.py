b75e640cbb68fbf4ef96d75b014272828c027423
Kevin <kevinacontrerasl62@gmail.com>
2020-12-08 23:08:38 -0600

    FINAL

--- archivoJSON.py
+++ archivoJSON.py
@@ -6,10 +6,10 @@ def archivojson(url):
   cadena3=""
   for trabajo in range(len(url[3])):
     diccionario  = {url[3][trabajo]:{ 'Candidatos':url[0][trabajo],'Edad Promedio':url[1][trabajo],'Pretension Salarial':url[2][trabajo]}}
-    j = json.dumps(diccionario,sort_keys=True, indent=7)
-    verdad = str(j)[1:-1]+","
+    j = json.dumps(diccionario,sort_keys=True, indent=4)
+    verdad = str(j)[1:-1].rstrip("\n")+","
     cadena =  cadena + verdad
   archivo = open("archivo.json","w")
-  archivo.write("{"+cadena.strip(",")+"}")
+  archivo.write("{"+cadena.strip(",")+"\n"+"}")
   archivo.close()
   os.system("archivo.json")
